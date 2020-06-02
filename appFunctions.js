ef85cdc612a2d9f935d115b6fbd723436c72017d
Paul Finegan <paul.s.finegan@gmail.com>
2020-06-02 20:33:12 +1000

    updated

--- appFunctions.js
+++ appFunctions.js
@@ -65,16 +65,24 @@ const getUserById = (userId,done) =>{
 	User.findById({_id:userId}).exec((err,data)=>{err ? done(err) : done(null,data)})
 }
 
+//get excercise by id
+const getExerciseById = (exId,done) => {
+		Exercise.findById({_id:exId}).populate('userID').exec((err,data)=>{
+			return err ? done(err) : done(null,{"userId":data.userID._id,"description":data.description,"duration":data.duration,"date":data.date.toDateString(),"username":data.userID.username})
+	})
+}
+
 //create new exercise function
 const newExerciseEntry = (userID,description,date,duration,done)=>{
-	const newExercise = new Exercise({userID:userID,description:description,date:date,duration:duration});
-	newExercise.save((err,data)=>{err ? done(err) : done(null,data)});
+	var newExercise = new Exercise({userID:userID,description:description,date:date,duration:duration});
+	newExercise.save((err,data)=>{
+		err ? done(err) : getExerciseById(data._id.toString(),(err,doc)=>{return err ? done(err) : done(null,doc)})
+	})
 }
 
 //query exercise function
 
 const findExercise = (userId,limit,from,to,done)=>{
-
 	Exercise.find({userID:userId,date:{$gte: from,$lt: to }}).populate('userID').limit(limit).exec((err,data)=>{
 		return err ? done(err) : done(null,data.map(e=> ({"userId":e.userID._id,"description":e.description,"duration":e.duration,"date":e.date.toDateString(),"username":e.userID.username}) ))
 	})
@@ -84,6 +92,7 @@ exports.User = User
 exports.getAllUsers = getAllUsers
 exports.getUserById = getUserById
 exports.Exercise = Exercise
+exports.getExerciseById = getExerciseById
 exports.createNewUser = createNewUser
 exports.newExerciseEntry = newExerciseEntry
 exports.findExercise = findExercise
